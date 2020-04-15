6867b9cfcc4b84d4ed8b94274bfbb5cf2a79c860
Noel Light-Hilary <noel@noellh.com>
2020-04-15 03:08:12 +0100

    Try further tweaking secret loading interpolation

--- deploy/secrets_entrypoint_tasks.sh
+++ deploy/secrets_entrypoint_tasks.sh
@@ -10,7 +10,7 @@ if [ -z "$SECRETS_BUCKET_NAME" ]; then
 fi
 
 # Load the S3 secrets file contents into the environment variables
-export $(aws s3 cp "s3://${SECRETS_BUCKET_NAME}/secrets" - | grep -v '^#' | xargs)
+export $(aws s3 cp s3://${SECRETS_BUCKET_NAME}/secrets - | grep -v '^#' | xargs)
 
 echo "Starting task..."
 # Call the normal CLI entry-point script, passing on script name and any other arguments
