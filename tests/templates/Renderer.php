bf6402989836fb0239c47dcd4c4252e04e8edbdb
Barney Laurance <barney@thebiggive.org.uk>
2023-10-10 15:53:01 +0100

    MAIL-47: Refactor, move twig test code into reusable function

---
+++ tests/templates/Renderer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mailer\Tests\templates;
+
+use Twig\Environment;
+use Twig\Extra\CssInliner\CssInlinerExtension;
+use Twig\Extra\Intl\IntlExtension;
+use Twig\Loader\FilesystemLoader;
+
+class Renderer
+{
+    public static function renderMessage(string $templateFileName, array $data): string
+    {
+        $twig = new Environment(new FilesystemLoader(__DIR__ . '/../../templates'));
+        $twig->addExtension(new CssInlinerExtension());
+        $twig->addExtension(new IntlExtension(
+            new \IntlDateFormatter('en-GB'),
+            new \NumberFormatter('en-GB', \NumberFormatter::CURRENCY)
+        ));
+
+        return $twig->render($templateFileName, $data);
+    }
+}
