138c4166f7483ac79d48613ddc11ff04eabf1be6
Barney Laurance <barney@thebiggive.org.uk>
2023-10-10 15:44:01 +0100

    MAIL-47: Add template for email to acknowledge reciept of donation funds

---
+++ tests/templates/DonorFundsThanksTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailer\Tests\templates;
+
+use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use Twig\Environment;
+use Twig\Extra\CssInliner\CssInlinerExtension;
+use Twig\Extra\Intl\IntlExtension;
+use Twig\Loader\FilesystemLoader;
+
+class DonorFundsThanksTest extends TestCase
+{
+    use MatchesSnapshots;
+
+    public function testItRendersEmailToThankDonorForFundingAnAccount(): void
+    {
+        $rendered = Renderer::renderMessage(
+            'donor-funds-thanks.html.twig',
+            [
+                'donorFirstName' => 'Josh',
+                // taking the money amounts as strings not numbers - although other emails such as
+                // donor-donation-success take money amounts as numbers and format them, I feel like its better to give
+                // the responsibility for formatting money amount as a string to the calling service e.g. matchbot.
+                'transferAmount' => "£500",
+                'newBalance' => "£15,000",
+            ]
+        );
+
+        $this->assertMatchesSnapshot($rendered);
+    }
+}
