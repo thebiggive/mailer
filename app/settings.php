795532c1e925364e8dd5e9df88cffb3e5d31fd61
Barney Laurance <barney@thebiggive.org.uk>
2025-10-29 11:16:48 +0000

    MAT-407: Add placeholder for regular giving payment failed email

--- app/settings.php
+++ app/settings.php
@@ -69,6 +69,14 @@ return function (ContainerBuilder $containerBuilder) {
                     ],
                 ],
                 [
+                    'templateKey' => 'donor-regular-donation-failed-payment',
+                    'subject' => "We couldn't collected your regular payment for %s ",
+                    'subjectParams' => ['charityName'],
+                    'requiredParams' => [
+                        'charityName',
+                    ],
+                ],
+                [
                     'templateKey' => 'donor-donation-refund-tip',
                     'subject' => 'Tip Refund',
                     'subjectParams' => [],
